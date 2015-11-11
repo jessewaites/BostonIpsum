8e4a5ca92c4198a3c29c25a537bdbbbd5ff423e9
jessewaites <jesse.waites@gmail.com>
2015-11-10 20:00:18 -0500

    hotfix

--- lib/boston_ipsum.rb
+++ lib/boston_ipsum.rb
@@ -3,7 +3,7 @@ require "boston_ipsum/version"
 module BostonIpsum
 
   def self.speak(integer)
-    if integer.is_a? Integer && integer > 0
+    if (integer.is_a? (Integer)) && integer > 0
       array = ["MGH", "Brady", "Cambridge", "Beantown", "Sox", "Patriots", "MIT", "Harvard", "Beacon Hill", "Dorchester", "Roxbury", 
         "Brahmin", "The Cape", "Martha's Vineyard", "Eastie", "Southie", "The North End", "Somerville", "MBTA", "JesseWaites.com"]
       if integer > (array.count - 1)
--- lib/boston_ipsum/version.rb
+++ lib/boston_ipsum/version.rb
@@ -1,3 +1,3 @@
 module BostonIpsum
-  VERSION = "1.5.0"
+  VERSION = "1.5.5"
 end
