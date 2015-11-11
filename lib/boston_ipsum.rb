d2ed2157c9529bef68ded85a924fb5d4b33426bd
jessewaites <jesse.waites@gmail.com>
2015-11-10 19:35:22 -0500

    repair minor error

--- lib/boston_ipsum.rb
+++ lib/boston_ipsum.rb
@@ -1,14 +1,15 @@
 require "boston_ipsum/version"
 
 module BostonIpsum
+
   def self.speak(integer)
     array = ["MGH", "Brady", "Cambridge", "Beantown", "Sox", "Patriots", "MIT", "Harvard", "Beacon Hill", "Dorchester", "Roxbury", 
       "Brahmin", "The Cape", "Martha's Vineyard", "Eastie", "Southie", "The North End", "Somerville", "MBTA"]
-      if integer > (array.count - 1)
-        array.join(" ")
-      else
-        string = array[0..(integer - 1)].shuffle.join(" ")
-      end
+    if integer > (array.count - 1)
+      array.join(" ")
+    else
+      string = array[0..(integer - 1)].shuffle.join(" ")
     end
   end
+
 end
